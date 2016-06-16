7ec8705d9414eb91551f408632dd588b395aefa7
admin <xuxt-314@qq.com>
2016-06-16 23:56:55 +0800

    ...

--- app/src/main/java/us/xingkong/xingpostcard/Key.java
+++ app/src/main/java/us/xingkong/xingpostcard/Key.java
@@ -7,6 +7,6 @@ package us.xingkong.xingpostcard;
 public class Key {
     public static int CoverFlow_Width = 800;
     public static int CoverFlow_height = 1000;
-    public static String UPDATEURL = "http://api.fir.im/apps/latest/573ecefa748aac36f4000007?api_token=";
+    public static String UPDATEURL  = "http://api.fir.im/apps/latest/573ecefa748aac36f4000007?api_token=";
 
 }
